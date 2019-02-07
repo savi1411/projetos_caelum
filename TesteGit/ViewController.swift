8b03a513dd9c721f3df7ff92182915c5ed391bd1
calsavi <carlosalberto.savi@gmail.com>
2019-02-06 22:45:28 -0200

    Initial Commit

---
+++ TesteGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteGit
+//
+//  Created by Carlos A Savi on 06/02/19.
+//  Copyright © 2019 savit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
